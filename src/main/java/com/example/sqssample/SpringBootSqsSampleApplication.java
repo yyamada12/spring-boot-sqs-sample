63b906d0422d8b42abe51837508c56907518650e
yyamada12 <12yacropolisy@gmail.com>
2023-05-28 11:38:48 +0900

    initial commit: spring initializr

---
+++ src/main/java/com/example/sqssample/SpringBootSqsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSqsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sqssample/SpringBootSqsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
